9d558f97c61e04dfd39183f868c40323a60d26a3
Suresh G <sgopal1@walmartlabs.com>
2015-09-17 01:53:35 -0700

    Kotlin Observable tests

--- src/main/kotlin/com/test/KTest.kt
+++ src/main/kotlin/com/test/KTest.kt
@@ -1,8 +1,14 @@
 package com.test
 
+import com.test.stream.RandomObservable
 import com.test.stream.RandomStream
+import rx.Observer
+import rx.schedulers.Schedulers
+import java.util.concurrent.CountDownLatch
 import java.util.concurrent.TimeUnit.*
+import java.util.concurrent.atomic.AtomicInteger
 import java.util.stream.IntStream
+import kotlin.concurrent.currentThread
 
 /**
  * Kotlin Sample class
@@ -41,4 +47,26 @@ fun main(args: Array<String>) {
     }.forEach { println(it) }
 
 
+    val o = RandomObservable()
+    var count = 0
+    val latch = CountDownLatch(1)
+
+    o.observeOn(Schedulers.computation()).limit(34).subscribe(object : Observer<Int> {
+        override fun onError(e: Throwable?) {
+            e?.printStackTrace()
+        }
+
+        override fun onNext(t: Int?) {
+            println("<<<-- $t - $currentThread")
+            count++
+        }
+
+        override fun onCompleted() {
+            println("Completed")
+            latch.countDown()
+        }
+
+    })
+    latch.await()
+    println("Got ${count} messages")
 }
